8e0004df8466956f4e49d5c0213385ff9be92961
Chandresh4997 <chandreshthakkar9090@gmail.com>
2026-05-29 18:59:16 +0530

    Updated locators file

--- tests/playwrightLocators.spec.ts
+++ tests/playwrightLocators.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('abc', async ({ page }) => {
+test('should broowse and add the product to cart', async ({ page }) => {
     await page.goto('https://www.fabindia.com/');
     await page.getByPlaceholder('Search here...').nth(0).fill('shirt');
     await page.keyboard.press('Enter');
 
     const [newPage] = await Promise.all([
-        page.context().waitForEvent('page'), // listens for new tab
-        page.getByAltText('Blue Cotton Slim Fit Shirt').click() // action that opens new tab
+        page.context().waitForEvent('page'), 
+        page.getByAltText('Blue Cotton Slim Fit Shirt').click() 
     ]);
     await newPage.locator('.custom_size_box').getByText('M', { exact: true }).click();
     await newPage.getByText('Add to cart ').click();
