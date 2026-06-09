7da3f3360e841426a7e67f41951ed3e3ec326e2c
Chandresh4997 <chandreshthakkar9090@gmail.com>
2026-06-09 17:00:25 +0530

    Added JSON login test data and updated tests to utilize it

--- tests/practice/csvLogin.spec.ts
+++ tests/practice/csvLogin.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { readCsv } from '../../utils/readCsv';
+import testFile from '../../data/testFile.json';
 
 let users: any[];
 
@@ -10,6 +11,18 @@ test.beforeAll(async () => {
 
 test('Login test using CSV data', async ({ page }) => {
   for (const user of users) {
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('#user-name').fill(user.username);
+    await page.locator('#password').fill(user.password);
+    await page.locator('#login-button').click();
+    await expect(page.locator('.app_logo')).toContainText('Swag Labs');
+    await page.pause();
+    console.log(`Login tested for: ${user.username}`);
+  }
+});
+
+test.only('Login test using JSON data', async ({ page }) => {
+  for (const user of testFile) {
     await page.goto('https://www.saucedemo.com/');
     await page.locator('#user-name').fill(user.username);
     await page.locator('#password').fill(user.password);
